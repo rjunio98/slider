20dc08fc16277b2798f1a3b1727dc28bc77a1a42
Robledo Junior <robledorjunior1@gmail.com>
2023-05-02 12:10:52 -0300

    primeiro commit

---
+++ src/js/slider.js
@@ -0,0 +1,60 @@
+const imagens = document.querySelectorAll(".slide")
+const setaVoltar = document.getElementById("seta-voltar")
+const setaAvancar = document.getElementById("seta-avancar")
+
+let imagemAtual = 0
+
+setaAvancar.addEventListener("click", function () {
+
+    if (imagemAtual === imagens.length - 1) {
+        return
+    }
+
+    esconderImagemAberta()
+
+    imagemAtual++
+
+    imagens[imagemAtual].classList.add("mostrar")
+
+    mostrarOuEsconderSetas()
+
+})
+
+setaVoltar.addEventListener("click", function () {
+
+    if (imagemAtual === 0) {
+        return
+    }
+
+    esconderImagemAberta()
+
+    imagemAtual--
+
+    imagens[imagemAtual].classList.add("mostrar")
+
+    mostrarOuEsconderSetas()
+})
+
+
+function esconderImagemAberta(){
+    const imagemAberta = document.querySelector(".mostrar")
+    imagemAberta.classList.remove("mostrar")
+}
+
+function mostrarOuEsconderSetas(){
+    const naoEhAPrimeiraImagem = imagemAtual !== 0
+    if (naoEhAPrimeiraImagem) {
+        setaVoltar.classList.remove("opacidade")
+    }else {
+        setaVoltar.classList.add("opacidade")
+    }
+
+    const chegounaUltimaImagem = imagemAtual !== 0 && imagemAtual === imagens.length - 1
+
+    if(chegounaUltimaImagem) {
+        setaAvancar.classList.add("opacidade")
+    }else {
+        setaAvancar.classList.remove("opacidade")
+    }
+}
+
